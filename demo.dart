3edd86fc0cdf6a42b11784a362033c050c702671
Lakmina99 <125084366+Lakmina99@users.noreply.github.com>
2023-04-06 10:07:08 +0530

    demo

---
+++ demo.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'demoData.dart';
+import 'diseasepage.dart';
+main(){
+  runApp(
+    MaterialApp(
+      home: SuggestionPage(),
+    ),
+  );
+}
+class SuggestionPage extends StatefulWidget {
+  const SuggestionPage({Key? key}) : super(key: key);
+
+  @override
+  State<SuggestionPage> createState() => _SuggestionPageState();
+}
+
+class _SuggestionPageState extends State<SuggestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(247, 255, 214, 190),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Column(
+          children: [
+            Text(
+              "Possible Suggestions",
+              style: TextStyle(color: Colors.black),
+            )
+          ],
+        ),
+      ),
+      
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                        return GestureDetector(
+                          onTap: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DiseasePage(
+                                  name : demoSuggestionData[3]['name'],
+                                  imagePath: demoSuggestionData[3]['image'],
+                                  description: demoSuggestionData[3]['des'],
+                                ),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            child: Suggestions(
+                              name: demoSuggestionData[3]['name'],
+                              imagePath: demoSuggestionData[3]['image'],
+                            ),
+                          ),
+                        );
+                      },
+                      childCount: 1,
+                    ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class Suggestions extends StatelessWidget {
+  final String name;
+  final String imagePath;
+  const Suggestions({
+    Key? key,
+    required this.name,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.all(24),
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+            image: AssetImage(imagePath),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.25),
+              BlendMode.darken,
+            )
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              name,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
